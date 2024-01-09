3e167c4846896c738b3c95ba378adffc1fca227f
Yunus Emre Durmuş <ysemredurmus@gmail.com>
2024-01-09 20:19:05 +0300

    Refactor code to improve performance and readability

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -18,136 +18,178 @@ class GelirDetay {
   GelirDetay(this.miktar, this.gelirTarihi);
 }
 
+class GiderDetay {
+  String miktar;
+  String giderTarihi;
+
+  GiderDetay(this.miktar, this.giderTarihi);
+}
+
 class _HomeScreenState extends State<HomeScreen> {
   var gelir = 0;
   List<String> gelirDetayListesi = [];
+  List<String> giderDetayListesi = [];
 
   bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Kişisel Cüzdan'),
-        ),
-        body: Stack(
-          children: [
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 380,
-                    child: GridView.count(
-                      physics: NeverScrollableScrollPhysics(),
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 20.0,
-                      mainAxisSpacing: 20.0,
-                      children: [
-                        GestureDetector(
-                          onTap: () async {
-                            await _animation();
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => CreditCardsScreen(),
+      appBar: AppBar(
+        title: Text('Kişisel Cüzdan'),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 380,
+                  child: GridView.count(
+                    physics: NeverScrollableScrollPhysics(),
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 20.0,
+                    mainAxisSpacing: 20.0,
+                    children: [
+                      GestureDetector(
+                        onTap: () async {
+                          await _animation();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => CreditCardsScreen(),
+                            ),
+                          );
+                        },
+                        child: _buildGridItem(
+                            Icons.credit_card, 'Kredi Kartları', Colors.red),
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          await _animation();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => InvoiceScreen(),
+                            ),
+                          );
+                        },
+                        child: _buildGridItem(
+                            Icons.inventory, 'Faturalar', Colors.red),
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          await _animation();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => IncomesScreen(
+                                gelirDetayListesi: gelirDetayListesi,
                               ),
-                            );
-                          },
-                          child: _buildGridItem(
-                              Icons.credit_card, 'Kredi Kartları'),
-                        ),
-                        GestureDetector(
-                          onTap: () async {
-                            await _animation(); // _animation'ı çağır
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => InvoiceScreen(),
-                              ),
-                            );
-                          },
-                          child: _buildGridItem(Icons.inventory, 'Faturalar'),
-                        ),
-                        GestureDetector(
-                          onTap: () async {
-                            await _animation();
-                            // ignore: use_build_context_synchronously
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => IncomesScreen(
-                                    gelirDetayListesi: gelirDetayListesi),
-                              ),
-                            );
-                          },
-                          child: _buildGridItem(Icons.money, 'Gelirlerim'),
-                        ),
-                        _buildGridItem(
-                            Icons.account_balance_wallet, 'Giderlerim'),
-                      ],
-                    ),
+                            ),
+                          );
+                        },
+                        child: _buildGridItem(
+                            Icons.money, 'Gelirlerim', Colors.green),
+                      ),
+                      _buildGridItem(Icons.account_balance_wallet, 'Giderlerim',
+                          Colors.orange),
+                    ],
                   ),
-                  _buildDivider(),
-                  SizedBox(height: 20),
-                  _buildBalanceRow(
-                    "Toplam Bakiye:",
-                    "${gelirDetayListesi.isNotEmpty ? gelirDetayListesi.map((e) => int.parse(e)).reduce((value, element) => value + element).toString() : "0.00"} ₺",
-                  ),
-                  SizedBox(height: 20),
-                  _buildBalanceRow("Toplam Gider:", "0.00 ₺"),
-                  SizedBox(height: 20),
-                  _buildBalanceRow("Toplam Gelir:", "0.00 ₺"),
-                  SizedBox(height: 20),
-                  _buildDivider(),
-                  SizedBox(height: 20),
-                ],
-              ),
-            ),
-            isLoading
-                ? Align(
-                    alignment: Alignment.center,
-                    child: Lottie.asset(
-                      'assets/animations/animation.json',
-                      width: 100,
-                      height: 100,
-                    ),
-                  )
-                : const SizedBox.shrink(),
-          ],
-        ),
-        floatingActionButton: Container(
-          width: 60,
-          height: 60,
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 68, 6, 201),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: FloatingActionButton(
-            backgroundColor: Color.fromARGB(255, 68, 6, 201),
-            onPressed: () => _gelirEkleDialog(context),
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.add, size: 30, color: Colors.white),
-                  Text(
-                    "Gelir",
-                    style: TextStyle(fontSize: 10, color: Colors.white),
-                  ),
-                ],
-              ),
+                ),
+                _buildDivider(),
+                SizedBox(height: 20),
+                _buildBalanceRow(
+                  "Toplam Bakiye:",
+                  "${gelirDetayListesi.isNotEmpty ? gelirDetayListesi.map((e) => int.parse(e)).reduce((value, element) => value + element).toString() : "0.00"} ₺",
+                ),
+                SizedBox(height: 20),
+                _buildBalanceRow("Toplam Gider:", "0.00 ₺"),
+                SizedBox(height: 20),
+                _buildBalanceRow("Toplam Gelir:", "0.00 ₺"),
+                SizedBox(height: 20),
+              ],
             ),
           ),
-        ));
+          isLoading
+              ? Align(
+                  alignment: Alignment.center,
+                  child: Lottie.asset(
+                    'assets/animations/animation.json',
+                    width: 100,
+                    height: 100,
+                  ),
+                )
+              : const SizedBox.shrink(),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            width: 70,
+            height: 55,
+            margin: EdgeInsets.only(right: 100),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 68, 6, 201),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: FloatingActionButton(
+              backgroundColor: Color.fromARGB(255, 68, 6, 201),
+              onPressed: () => _gelirEkleDialog(context),
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.add, size: 30, color: Colors.white),
+                    Text(
+                      "Gelir",
+                      style: TextStyle(fontSize: 10, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 70,
+            height: 55,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 68, 6, 201),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: FloatingActionButton(
+              backgroundColor: Color.fromARGB(255, 68, 6, 201),
+              onPressed: () => _gelirEkleDialog(context),
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.add, size: 30, color: Colors.white),
+                    Text(
+                      "Gelir",
+                      style: TextStyle(fontSize: 10, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
-  Widget _buildGridItem(IconData icon, String text) {
+  Widget _buildGridItem(IconData icon, String text, Color color) {
     return Container(
       decoration: BoxDecoration(
-        color: Color.fromARGB(255, 68, 6, 201),
+        color: color,
         borderRadius: BorderRadius.circular(20),
       ),
       child: Column(
@@ -172,9 +214,9 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildDivider() {
-    return Divider(
+    return const Divider(
       height: 20,
-      color: Color.fromARGB(255, 68, 6, 201),
+      color: Colors.amber,
       thickness: 2,
     );
   }
@@ -229,7 +271,7 @@ class _HomeScreenState extends State<HomeScreen> {
   void _gelirEkle() {
     setState(() {
       GelirDetay yeniGelirDetay =
-          new GelirDetay(gelir.toString(), DateTime.now().toString());
+          GelirDetay(gelir.toString(), DateTime.now().toString());
 
       // Yeni gelir detayını listeye ekleyin
       gelirDetayListesi.add(yeniGelirDetay.miktar);
@@ -246,6 +288,16 @@ class _HomeScreenState extends State<HomeScreen> {
       setState(() {
         isLoading = false;
       });
+    });
+  }
+
+  void _giderEkle() {
+    setState(() {
+      GiderDetay yeniGiderDetay =
+          GiderDetay(gelir.toString(), DateTime.now().toString());
+
+      // Yeni gider detayını listeye ekleme
+      giderDetayListesi.add(yeniGiderDetay.miktar);
     });
   }
 }
